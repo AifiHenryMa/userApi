f0cafdec9ea1c3f441368be66c0f6a7e7a73b83b
AifiHenryMa <36319820+AifiHenryMa@users.noreply.github.com>
2018-02-11 16:15:40 +0800

    userApi 的测试demo

---
+++ testUserApi.js
@@ -0,0 +1,10 @@
+userApi = require('./userApi');
+
+// 注册功能
+userApi.register('Shangsdfgdfjsgbdjfjihkgjlkljhklhai', 'www.baidu.com', function () { });
+
+// 登陆功能
+userApi.logIn('Shanghai', 'www.baidu.com', function () { });
+
+// 登出功能
+userApi.logOut('Shanghai', 'www.baidu.com', function () { });
